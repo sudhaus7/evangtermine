5533041091efa70d1ae153fee45388a107c75902
Daniel Simon <dsimon@sudhaus7.de>
2025-09-22 09:31:58 +0200

    PageNotFoundMiddleware.php: if event doesn't exist anymore, redirect to events list

---
+++ Classes/Middleware/PageNotFoundMiddleware.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace ArbkomEKvW\Evangtermine\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Http\Uri;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class PageNotFoundMiddleware implements MiddlewareInterface
+{
+    protected array $uriParts = [
+        '/termindetails/',
+        '/terminteaser/',
+    ];
+    protected string $requestUri = '';
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        /**
+         * If it's the 404 error page, check if it's the detail page of an event.
+         * If true, redirect to the event list page
+         */
+        if ($this->is404Request($request)) {
+            foreach ($this->uriParts as $uriPart) {
+                if (str_contains($this->requestUri, $uriPart)) {
+                    $uriArray = explode($uriPart, $this->requestUri);
+                    if (count($uriArray) > 1) {
+                        $uri = $this->createUriString($uriArray[0], $request);
+                        $newUri = GeneralUtility::makeInstance(Uri::class, $uri);
+
+                        /** @var ServerRequest $newRequest */
+                        $newRequest = GeneralUtility::makeInstance(ServerRequest::class,
+                            $newUri,
+                            $request->getMethod(),
+                            $request->getBody(),
+                            $request->getHeaders(),
+                            $request->getServerParams()
+                        );
+                        foreach ($request->getAttributes() as $key => $attribute) {
+                            $newRequest = $newRequest->withAttribute($key, $attribute);
+                        }
+                        return $handler->handle($newRequest);
+                    }
+                }
+            }
+        }
+        return $handler->handle($request);
+    }
+
+    protected function is404Request(ServerRequestInterface $request): bool
+    {
+        $path = $request->getUri()->getPath();
+        $serverParams = $request->getServerParams();
+        $this->requestUri = $serverParams['REQUEST_URI'] ?? '';
+        if (!empty($this->requestUri) && $path !== $this->requestUri) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param $uriArray
+     * @param ServerRequestInterface $request
+     * @return string
+     */
+    protected function createUriString($uriArray, ServerRequestInterface $request): string
+    {
+        $uri = $uriArray;
+        $uri = $request->getUri()->getHost() . $uri;
+        $uri = str_replace('//', '/', $uri);
+        return $request->getUri()->getScheme() . '://' . $uri;
+    }
+}
---
+++ Configuration/RequestMiddlewares.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the TYPO3 project.
+ * (c) 2022 B-Factor GmbH
+ *          Sudhaus7
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ * The TYPO3 project - inspiring people to share!
+ * @copyright 2022 B-Factor GmbH https://b-factor.de/
+ * @author Frank Berger <fberger@b-factor.de>
+ * @author Daniel Simon <dsimon@b-factor.de>
+ */
+
+return [
+    'frontend' => [
+        'sudhaus7/evangtermine' => [
+            'target' => \ArbkomEKvW\Evangtermine\Middleware\PageNotFoundMiddleware::class,
+            'after' => [
+                'typo3/cms-frontend/eid',
+            ],
+            'before' => [
+                'typo3/cms-frontend/site',
+            ],
+        ],
+    ],
+];
